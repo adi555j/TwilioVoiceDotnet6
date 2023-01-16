d0092e74bd75b15f6ad2a5168fa50a3382685a48
apushkar <adi555j@gmail.com>
2023-01-16 22:13:27 +0530

    Added Initial files

---
+++ Dotnet6TwilioVoice/AppConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Dotnet6TwilioVoice
+{
+    public class AppConfiguration
+    {
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+        public string Number { get; set; }
+    }
+}
+
---
+++ Dotnet6TwilioVoice/Controllers/CallingController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.AspNet.Core;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
+
+namespace Dotnet6TwilioVoice.Controllers
+{
+    public class CallingController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CallingController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // Get: ReceiveCall
+        public async Task<IActionResult> ReceiveCall()
+        {
+            var twiml = new VoiceResponse();
+            var response = new TwiMLResult(twiml.Say("You are calling Marcos Placona").Dial(_configuration["Twilio:Number"]));
+            return response;
+        }
+
+        // POST: MakeCall
+        [HttpGet]
+        public async Task<IActionResult> MakeCall()
+        {
+            // Instantiate new Rest API object
+
+            TwilioClient.Init(_configuration["Twilio:AccountSid"], _configuration["Twilio:AuthToken"]);
+            var call = CallResource.Create(
+                twiml: new Twilio.Types.Twiml("<Response><Say>Ahoy, World!</Say></Response>"),
+                to: new Twilio.Types.PhoneNumber(_configuration["Twilio:Number"]),
+                from: new Twilio.Types.PhoneNumber("")
+            );
+
+            return Content("The call has been is initiated");
+        }
+    }
+}
+
